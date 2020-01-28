5e7aa1e7d7520b90a5ef7743149142544330d7e7
Josh Ford <thisisjoshford@gmail.com>
2020-01-28 10:52:52 -0800

    added value assignment to radio button in app.js

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ import BusMallProductArray from './data/productArray.js';
 //grabs all of the radio tag input elements from DOM
 const bmProductSelection = document.querySelectorAll('input');
 
+let numberOfClicks = 0;
 //adds event listener to each radio image button
 bmProductSelection.forEach((inputTag) => {
     inputTag.addEventListener('click', () => {
+        numberOfClicks++;
         generateRandomBM();
+        console.log(inputTag.value);
+        console.log(numberOfClicks);
     });
 });
 
@@ -34,18 +38,27 @@ function generateRandomBM() {
     const randomBM1object = (bmProducts.getById(randomBM1.id));
     const randomBM2object = (bmProducts.getById(randomBM2.id));
     const randomBM3object = (bmProducts.getById(randomBM3.id));
-    //populates the DOM with selected name and IMG
+    //populates the DOM with selected name, image, and value id (for data)
     const bmProductName1 = document.getElementById('bmProductName1');
     bmProductName1.textContent = randomBM1object.name;
     const bmProductImage1 = document.getElementById('randomImg1');
     bmProductImage1.src = randomBM1object.image;
+    const bmProductValue1 = document.getElementById('randomInput1');
+    bmProductValue1.value = randomBM1object.id;
+   
     const bmProductName2 = document.getElementById('bmProductName2');
     bmProductName2.textContent = randomBM2object.name;
     const bmProductImage2 = document.getElementById('randomImg2');
     bmProductImage2.src = randomBM2object.image;
+    const bmProductValue2 = document.getElementById('randomInput2');
+    bmProductValue2.value = randomBM2object.id;
+    
     const bmProductName3 = document.getElementById('bmProductName3');
     bmProductName3.textContent = randomBM3object.name;
     const bmProductImage3 = document.getElementById('randomImg3');
     bmProductImage3.src = randomBM3object.image;
+    const bmProductValue3 = document.getElementById('randomInput3');
+    bmProductValue3.value = randomBM3object.id;
+    
     return { randomBM1object, randomBM2object, randomBM3object, randomBM1, randomBM2, randomBM3 };
 }
