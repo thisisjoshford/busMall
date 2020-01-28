d9b0736202fb32ee8b4c29b71d84988e8c45c0d3
Josh Ford <thisisjoshford@gmail.com>
2020-01-27 21:49:34 -0800

    random images populating to DOM

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import busMallProducts from './data/data.js';
 //links constructor routine from productArray
 import BusMallProductArray from './data/productArray.js';
 
+
+
 //creates a variable that has all of the BusMall Products
 const bmProducts = new BusMallProductArray(busMallProducts);
 
@@ -18,6 +20,25 @@ while (randomBM1.id === randomBM3.id || randomBM2.id === randomBM3.id) {
     randomBM3 = bmProducts.getRandomProduct();
 }
 
+const randomBM1object = (bmProducts.getById(randomBM2.id));
+const randomBM2object = (bmProducts.getById(randomBM2.id));
+const randomBM3object = (bmProducts.getById(randomBM3.id));
+
+const bmProductImage1 = document.getElementById('randomImg1');
+bmProductImage1.src = randomBM1object.image;
+bmProductImage1.width = 300;
+
+const bmProductImage2 = document.getElementById('randomImg2');
+bmProductImage2.src = randomBM2object.image;
+bmProductImage2.width = 300;
+
+
+const bmProductImage3 = document.getElementById('randomImg3');
+bmProductImage3.src = randomBM3object.image;
+bmProductImage3.width = 300;
+
+
+console.log(randomBM1object.image);
 console.log(randomBM1.id);
 console.log(randomBM2.id);
 console.log(randomBM3.id);
