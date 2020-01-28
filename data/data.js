8777f742eb1384183fe470a4c9e958b82a4444d9
Josh Ford <thisisjoshford@gmail.com>
2020-01-27 19:07:20 -0800

    product array complete

---
+++ data/data.js
@@ -0,0 +1,103 @@
+export const busMallProducts = [
+    { 
+        id: 1,
+        name: 'bag',
+        image: '../assets/bag.jpg'
+    },
+    { 
+        id: 2,
+        name: 'banana',
+        image: '../assets/banana.jpg'
+    },
+    { 
+        id: 3,
+        name: 'bathroom',
+        image: '../assets/bathroom.jpg'
+    },
+    { 
+        id: 4,
+        name: 'boots',
+        image: '../assets/boots.jpg'
+    },
+    { 
+        id: 5,
+        name: 'breakfast',
+        image: '../assets/breakfast.jpg'
+    },
+    { 
+        id: 6,
+        name: 'bubblegum',
+        image: '../assets/bubblegum.jpg'
+    },
+    { 
+        id: 7,
+        name: 'chair',
+        image: '../assets/chair.jpg'
+    },
+    { 
+        id: 8,
+        name: 'cthulhu',
+        image: '../assets/cthulhu.jpg'
+    },
+    { 
+        id: 9,
+        name: 'dog duck',
+        image: '..assets/dog-duck.jpg'
+    },
+    { 
+        id: 10,
+        name: 'dragon',
+        image: '../assets/dragon.jpg'
+    },
+    { 
+        id: 11,
+        name: 'pen',
+        image: '../assets/pen.jpg'
+    },
+    { 
+        id: 12,
+        name: 'pet sweep',
+        image: '../assets/pet-sweep.jpg'
+    },
+    { 
+        id: 13,
+        name: 'scissors',
+        image: '../assets/scissors.jpg'
+    },
+    { 
+        id: 14,
+        name: 'shark',
+        image: '../assets/shark.jpg'
+    },  
+    { 
+        id: 15,
+        name: 'sweep',
+        image: '../assets/sweep.jpg'
+    },
+    { 
+        id: 16,
+        name: 'tauntaun',
+        image: '../assets/tauntaun.jpg'
+    },
+    { 
+        id: 17,
+        name: 'unicorn',
+        image: '../assets/unicorn.jpg'
+    },
+    { 
+        id: 18,
+        name: 'usb',
+        image: '../assets/usb.jpg'
+    },
+    { 
+        id: 19,
+        name: 'water can',
+        image: '../assets/water-can.jpg'
+    },
+    { 
+        id: 20,
+        name: 'wine glass',
+        image: '../assets/wine-glass.jpg'
+    },
+
+];
