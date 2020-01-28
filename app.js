6f78226fa6324ab9e153a444cba3d069d9e2e31b
Josh Ford <thisisjoshford@gmail.com>
2020-01-28 09:44:17 -0800

    created a generate random BM function

--- app.js
+++ app.js
@@ -3,43 +3,46 @@ import busMallProducts from './data/data.js';
 //links constructor routine from productArray
 import BusMallProductArray from './data/productArray.js';
 
-
+//grabs all of the radio tag input elements from DOM
+const bmProductSelection = document.querySelectorAll('input');
 
 //creates a variable that has all of the BusMall Products
 const bmProducts = new BusMallProductArray(busMallProducts);
 
-//creating a random product
-const randomBM1 = bmProducts.getRandomProduct();
-//creates a second random product that will check if it matches with first...if so re-create a random product
-let randomBM2 = bmProducts.getRandomProduct();
-while (randomBM1.id === randomBM2.id) {
-    randomBM2 = bmProducts.getRandomProduct();}
-//same as above routine, but checks against random1 OR (||) random2
-let randomBM3 = bmProducts.getRandomProduct();
-while (randomBM1.id === randomBM3.id || randomBM2.id === randomBM3.id) {
-    randomBM3 = bmProducts.getRandomProduct();
+
+var { randomBM1object, randomBM2object, randomBM3object, randomBM1, randomBM2, randomBM3 } = generateRandomBM();
+//this function generates three random unique ID's... matches them to corresponding data array value... then populates the results to the ODM 
+function generateRandomBM() {
+    const randomBM1 = bmProducts.getRandomProduct();
+    //creates a second random product that will check if it matches with first...if so re-create a random product
+    let randomBM2 = bmProducts.getRandomProduct();
+    while (randomBM1.id === randomBM2.id) {
+        randomBM2 = bmProducts.getRandomProduct();
+    }
+    //same as above routine, but checks against random1 OR (||) random2
+    let randomBM3 = bmProducts.getRandomProduct();
+    while (randomBM1.id === randomBM3.id || randomBM2.id === randomBM3.id) {
+        randomBM3 = bmProducts.getRandomProduct();
+    }
+    //creates variables with object containers 
+    const randomBM1object = (bmProducts.getById(randomBM1.id));
+    const randomBM2object = (bmProducts.getById(randomBM2.id));
+    const randomBM3object = (bmProducts.getById(randomBM3.id));
+    //populates the DOM with selected name and IMG
+    const bmProductName1 = document.getElementById('bmProductName1');
+    bmProductName1.textContent = randomBM1object.name;
+    const bmProductImage1 = document.getElementById('randomImg1');
+    bmProductImage1.src = randomBM1object.image;
+    const bmProductName2 = document.getElementById('bmProductName2');
+    bmProductName2.textContent = randomBM2object.name;
+    const bmProductImage2 = document.getElementById('randomImg2');
+    bmProductImage2.src = randomBM2object.image;
+    const bmProductName3 = document.getElementById('bmProductName3');
+    bmProductName3.textContent = randomBM3object.name;
+    const bmProductImage3 = document.getElementById('randomImg3');
+    bmProductImage3.src = randomBM3object.image;
+    return { randomBM1object, randomBM2object, randomBM3object, randomBM1, randomBM2, randomBM3 };
 }
-//creates variables with object containers 
-const randomBM1object = (bmProducts.getById(randomBM1.id));
-const randomBM2object = (bmProducts.getById(randomBM2.id));
-const randomBM3object = (bmProducts.getById(randomBM3.id));
-
-
-const bmProductName1 = document.getElementById('bmProductName1');
-bmProductName1.textContent = randomBM1object.name;
-const bmProductImage1 = document.getElementById('randomImg1');
-bmProductImage1.src = randomBM1object.image;
-
-const bmProductName2 = document.getElementById('bmProductName2');
-bmProductName2.textContent = randomBM2object.name;
-const bmProductImage2 = document.getElementById('randomImg2');
-bmProductImage2.src = randomBM2object.image;
-
-const bmProductName3 = document.getElementById('bmProductName3');
-bmProductName3.textContent = randomBM3object.name;
-const bmProductImage3 = document.getElementById('randomImg3');
-bmProductImage3.src = randomBM3object.image;
-
 console.log(randomBM1object.image);
 console.log(randomBM2object.image);
 console.log(randomBM3object.image);
